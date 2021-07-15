4a3092d9d93f181e37f70014e27cbde49c22dd5c
Blank Devs <215006818@student.uj.ac.za>
2021-07-15 23:20:12 +0200

    cleaned up ui

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,4 @@
-// Card Hover Animation
-
-
-// Next screen
-function nextScreen(){
-	alert("I am an alert box!");
-};
+// Tel Number Picker
 
 
 // Line Chart
@@ -15,7 +9,7 @@ new Chart(document.getElementById("line-chart"), {
       datasets: [{ 
           data: [43, 11, 67, 98, 52, 89, 77, 40, 80, 90, 65, 29],
           label: "University of Johannesburg",
-          borderColor: "#3e95cd",
+          borderColor: "#FE1ACE",
           fill: false
         },
       ]
@@ -36,11 +30,11 @@ new Chart(document.getElementById("bar-chart-grouped"), {
       datasets: [
         {
           label: "Africa",
-          backgroundColor: "#3e95cd",
+          backgroundColor: "#111",
           data: [133,221,783,2478]
         }, {
           label: "Europe",
-          backgroundColor: "#8e5ea2",
+          backgroundColor: "#fece1a",
           data: [408,547,675,734]
         }
       ]
@@ -61,8 +55,8 @@ new Chart(document.getElementById("bar-chart-horizontal"), {
       datasets: [
         {
           label: "Population (millions)",
-          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-          data: [2478,5267,734,784,433]
+          backgroundColor: ["#CEFE1A", "#111","#FF33A5","#FECE1A","#FFC300"],
+          data: [2478,5267,734,1784,433]
         }
       ]
     },
@@ -74,42 +68,3 @@ new Chart(document.getElementById("bar-chart-horizontal"), {
       }
     }
 });
-
-// 
-google.charts.load('current', {packages: ['corechart', 'line']});
-google.charts.setOnLoadCallback(drawBasic);
-
-function drawBasic() {
-
-      var data = new google.visualization.DataTable();
-      data.addColumn('number', 'X');
-      data.addColumn('number', 'Dogs');
-
-      data.addRows([
-        [0, 0],   [1, 10],  [2, 23],  [3, 17],  [4, 18],  [5, 9],
-        [6, 11],  [7, 27],  [8, 33],  [9, 40],  [10, 32], [11, 35],
-        [12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
-        [18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
-        [24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
-        [30, 55], [31, 60], [32, 61], [33, 59], [34, 62], [35, 65],
-        [36, 62], [37, 58], [38, 55], [39, 61], [40, 64], [41, 65],
-        [42, 63], [43, 66], [44, 67], [45, 69], [46, 69], [47, 70],
-        [48, 72], [49, 68], [50, 66], [51, 65], [52, 67], [53, 70],
-        [54, 71], [55, 72], [56, 73], [57, 75], [58, 70], [59, 68],
-        [60, 64], [61, 60], [62, 65], [63, 67], [64, 68], [65, 69],
-        [66, 70], [67, 72], [68, 75], [69, 80]
-      ]);
-
-      var options = {
-        hAxis: {
-          title: 'Time'
-        },
-        vAxis: {
-          title: 'Popularity'
-        }
-      };
-
-      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-
-      chart.draw(data, options);
-    }
