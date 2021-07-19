41e0ad4a915894c4604ce6e725df82f00008f7fb
Blank Devs <215006818@student.uj.ac.za>
2021-07-19 02:25:19 +0200

    Added Plotly.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,70 +1,44 @@
-// Tel Number Picker
+// Plotly
+var data = [{
+  values: [19, 26, 55],
+  labels: ['Residential', 'Non-Residential', 'Utility'],
+  type: 'pie'
+}];
 
+var layout = {
+  height: 400,
+  width: 500
+};
 
-// Line Chart
-new Chart(document.getElementById("line-chart"), {
-    type: 'line',
-    data: {
-      labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
-      datasets: [{ 
-          data: [43, 11, 67, 98, 52, 89, 77, 40, 80, 90, 65, 29],
-          label: "University of Johannesburg",
-          borderColor: "#FE1ACE",
-          fill: false
-        },
-      ]
-    },
-    options: {
-      title: {
-        display: true,
-        text: 'Overall satisfaction by month'
-      }
-    }
-  });
+Plotly.newPlot('myDiv', data, layout);
 
-//   Bar Chart
-new Chart(document.getElementById("bar-chart-grouped"), {
-    type: 'bar',
-    data: {
-      labels: ["1900", "1950", "1999", "2050"],
-      datasets: [
-        {
-          label: "Africa",
-          backgroundColor: "#111",
-          data: [133,221,783,2478]
-        }, {
-          label: "Europe",
-          backgroundColor: "#fece1a",
-          data: [408,547,675,734]
-        }
-      ]
-    },
-    options: {
-      title: {
-        display: true,
-        text: 'Population growth (millions)'
-      }
-    }
-});
+// Scatter
+var trace1 = {
+  x: [1, 2, 3, 4],
+  y: [10, 15, 13, 17],
+  mode: 'markers',
+  type: 'scatter'
+};
 
-// Horizontal Bar Chart
-new Chart(document.getElementById("bar-chart-horizontal"), {
-    type: 'horizontalBar',
-    data: {
-      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
-      datasets: [
-        {
-          label: "Population (millions)",
-          backgroundColor: ["#CEFE1A", "#111","#FF33A5","#FECE1A","#FFC300"],
-          data: [2478,5267,734,1784,433]
-        }
-      ]
-    },
-    options: {
-      legend: { display: false },
-      title: {
-        display: true,
-        text: 'Predicted world population (millions) in 2050'
-      }
-    }
-});
+var trace2 = {
+  x: [2, 3, 4, 5],
+  y: [16, 5, 11, 9],
+  mode: 'lines',
+  type: 'scatter'
+};
+
+var trace3 = {
+  x: [1, 2, 3, 4],
+  y: [12, 9, 15, 12],
+  mode: 'lines+markers',
+  type: 'scatter'
+};
+
+var layout = {
+  height: 400,
+  width: 500
+};
+
+var data = [trace1, trace2, trace3];
+
+Plotly.newPlot('myScatter', data, layout);
