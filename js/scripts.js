37e8517cf20a6e3bf1bb3c16ce23c4d90bda2954
Blank Devs <215006818@student.uj.ac.za>
2021-07-19 09:33:20 +0200

    added all plotly graphs on overview

--- js/scripts.js
+++ js/scripts.js
@@ -42,3 +42,78 @@ var layout = {
 var data = [trace1, trace2, trace3];
 
 Plotly.newPlot('myScatter', data, layout);
+
+// Bar Chart
+var trace1 = {
+  x: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  y: [20, 14, 25, 16, 18, 22, 19, 15, 12, 16, 14, 17],
+  type: 'bar',
+  name: 'Primary Product',
+  marker: {
+    color: 'rgb(49,130,189)',
+    opacity: 0.7,
+  }
+};
+
+var trace2 = {
+  x: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  y: [19, 14, 22, 14, 16, 19, 15, 14, 10, 12, 12, 16],
+  type: 'bar',
+  name: 'Secondary Product',
+  marker: {
+    color: 'rgb(204,24,204)',
+    opacity: 0.5
+  }
+};
+
+var data = [trace1, trace2];
+
+var layout = {
+  title: '2013 Sales Report',
+  xaxis: {
+    tickangle: -45
+  },
+  barmode: 'group',
+  height: 400,
+  width: 500
+};
+
+
+Plotly.newPlot('myBar', data, layout);
+
+// Sunburst
+var data = [{
+  type: "sunburst",
+  ids: [
+    "North America", "Europe", "Australia", "North America - Football", "Soccer",
+    "North America - Rugby", "Europe - Football", "Rugby",
+    "Europe - American Football","Australia - Football", "Association",
+    "Australian Rules", "Autstralia - American Football", "Australia - Rugby",
+    "Rugby League", "Rugby Union"
+  ],
+  labels: [
+    "North<br>America", "Europe", "Australia", "Football", "Soccer", "Rugby",
+    "Football", "Rugby", "American<br>Football", "Football", "Association",
+    "Australian<br>Rules", "American<br>Football", "Rugby", "Rugby<br>League",
+    "Rugby<br>Union"
+  ],
+  parents: [
+    "", "", "", "North America", "North America", "North America", "Europe",
+    "Europe", "Europe","Australia", "Australia - Football", "Australia - Football",
+    "Australia - Football", "Australia - Football", "Australia - Rugby",
+    "Australia - Rugby"
+  ],
+  outsidetextfont: {size: 20, color: "#377eb8"},
+  // leaf: {opacity: 0.4},
+  marker: {line: {width: 2}},
+}];
+
+var layout = {
+  margin: {l: 0, r: 0, b: 0, t:0},
+  sunburstcolorway:["#fece1a","#6faabc","#303030"],
+  height: 400,
+  width: 500
+};
+
+
+Plotly.newPlot('mySun', data, layout);
